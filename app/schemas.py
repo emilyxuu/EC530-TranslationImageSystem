1e7e387e1a1a9f0da4d18218b7f649b248b3a37f
aylatabi <ayta5739@colorado.edu>
2026-04-21 20:31:46 -0400

    Added isvalidevent to schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -26,3 +26,26 @@ def create_base_event(topic: str, image_id: str, path: str, source: str) -> dict
             "source": source
         }
     }
+    
+def is_valid_event(event):
+    
+    #check if the event is a dictionary, if not return 
+    if not isinstance(event, dict):
+       return False
+   
+    #need to valid if all fields are in event if not then return
+    required = ["topic", "event_id", "timestamp", "payload"]
+    for field in required:
+        if field not in event:
+            return False
+        
+    #check if the values in event are valid, topic, event_id and timestamp must be strings   
+    str_required = ["topic", "event_id", "timestamp"]   
+    for field in str_required:
+        if not isinstance(event[field], str) or event[field] == "": 
+            return False
+    #payload must be a dict
+    if not isinstance(event["payload"], dict):
+        return False
+    #if everything checks out then return True
+    return True
